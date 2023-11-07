54d89d9c87ddbd251bdb5aa74471bfea8ad53b34
CheeJiaXu <114555103+CheeJiaXu@users.noreply.github.com>
2023-11-07 12:59:44 +1100

    made into function

--- Computer Vision/colour_detection.py
+++ Computer Vision/colour_detection.py
@@ -34,26 +34,30 @@ def colour_detect(img):
     
     return vector
 
-cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+def laptop_video_test():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
 
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
 
-    print(colour_detect(frame))
+        print("vector: " + colour_detect(frame))
 
-    if cv2.waitKey(1) == ord('q'):
-        break
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+        if cv2.waitKey(1) == ord('q'):
+            break
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    laptop_video_test()
