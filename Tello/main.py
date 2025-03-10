6cc619c49d0349cdbb696c3a5cef4f5a41dc869f
jamesolee <jollee8013@gmail.com>
2025-03-11 10:59:53 +1100

    main.py created for testing tello
    Added Webcam and Flight modes as testing options, timeout for safety when flying, global maximum velocity, state machine

---
+++ Tello/main.py
@@ -0,0 +1,139 @@
+from djitellopy import Tello
+import cv2
+import numpy as np
+from video_to_gate_centre import gate_position_cf
+import time
+
+# Set this to true if testing on webcam
+WEBCAM = False
+# Set this to True to enable takeoff and gate traversal
+FLIGHT = False
+
+# Seconds for test flight before auto-land
+TIMEOUT_STOP = 20
+
+# Ensure linear velocity is below this for safety
+V_PERCENT_MAX = 10
+
+# State machine states
+S0_IDLE = 0
+S1_NO_GATE = 1
+S2_CENTRE_GATE = 2
+gate_centred = False
+S3_TRAVERSE = 3
+
+state = S0_IDLE
+
+aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+parameters = cv2.aruco.DetectorParameters()
+detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+mat = np.array([[921.170702, 0.000000, 459.904354], [0.000000, 919.018377, 351.238301], [0.000000, 0.000000, 1.000000]])
+dist = np.array([-0.033458, 0.105152, 0.001256, -0.006647, 0.000000])
+
+if WEBCAM:
+    cam = cv2.VideoCapture(1)
+else:
+    tello = Tello()
+    tello.connect()
+    tello.streamon()
+    frame_read = tello.get_frame_read()
+
+if FLIGHT:
+    tello.takeoff()
+    tello.move_up(20)
+    t_takeoff = time.time()
+
+height = 0
+
+while True:
+
+    # Read camera and process frame as required
+    if WEBCAM:
+        ret, img = cam.read()
+    else:
+        img = frame_read.frame
+        height = tello.get_height()
+    if img is None:
+        raise ValueError("frame not loaded.")
+    if not WEBCAM:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Look for aruco markers
+    corners, ids, rejected = detector.detectMarkers(gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # State transitions
+    if state == S0_IDLE:
+        state = S1_NO_GATE
+
+    elif state == S1_NO_GATE:
+        if len(corners) >= 2:
+            state = S2_CENTRE_GATE
+    
+    elif state == S2_CENTRE_GATE:
+        if gate_centred:
+            gate_centred = False
+            state = S3_TRAVERSE
+
+    elif state == S3_TRAVERSE:
+        pass
+    
+    translate = np.array([0,0])
+    if len(corners) >= 2:
+        gate_centre, pt1, pt2 = gate_position_cf(corners)
+        # Draw line to gate centre
+        video_centre = np.array([gray.shape[1],gray.shape[0]])//2
+        img = cv2.line(img, video_centre, gate_centre, color=(0,0,255), thickness=5)
+        img = cv2.circle(img, gate_centre,10, color=(0,255,0),thickness = -1)
+        
+        img = cv2.circle(img, pt1, 10, color=(255,0,0),thickness = -1)
+        img = cv2.circle(img, pt2, 10, color=(0,0,255),thickness = -1)
+
+        translate = gate_centre - video_centre
+        print(f'Translate: {translate}')
+
+    cv2.imshow('Detected Markers', img)
+    
+
+    if FLIGHT:
+        print(f'state: {state} t: {time.time()-t_takeoff:.2f}s')
+        if time.time() - t_takeoff >= TIMEOUT_STOP:
+            print('Flight time over - landing...')
+            break
+
+    if S1_NO_GATE:
+        # Spin until gate found
+
+        v_lr = 0 # left_right_velocity
+        v_fb = 0 # forward_backward_velocity
+        v_ud = 0 # up_down_velocity
+        w_yaw = 20 # yaw_velocity
+        tello.send_rc_control( int(v_lr), int(v_fb), int(v_ud), int(w_yaw))
+    
+    # P-control navigation to centre the gate
+    Kp = 0.08 # Proportional gain
+
+    if state == S2_CENTRE_GATE:
+        if len(corners) >= 2:
+            # Update velocity RC channels -100 to 100
+            v_lr = np.sign(translate[0]) * min(Kp*abs(translate[0]),V_PERCENT_MAX) # left_right_velocity
+            v_fb = 10 # forward_backward_velocity
+            v_ud = 0 # up_down_velocity
+            w_yaw = 0 # yaw_velocity
+        print(f'v_lr = {v_lr}')
+        tello.send_rc_control( int(v_lr), int(v_fb), int(v_ud), int(w_yaw))
+
+
+
+# Exit gracefully
+if WEBCAM:
+    pass
+else:
+    tello.streamoff()
+
+if FLIGHT:
+    tello.land()
+
+    
--- Tello/video_to_gate_centre.py
+++ Tello/video_to_gate_centre.py
@@ -1,23 +1,40 @@
 import numpy as np
 
-corners = np.array([[[389., 81.],[407., 81.],[407., 97.],[389., 98.]], [[300., 80.],[400., 80.],[400., 100.],[390., 100.]]])
+# corners = np.array([[[389., 81.],[407., 81.],[407., 97.],[389., 98.]], [[300., 80.],[400., 80.],[400., 100.],[390., 100.]]])
 
 def gate_position_cf(corners):
-    # define the video dimensions (720p) and centre
-    video_dim = np.array([1280, 720])
-    video_centre = video_dim / 2
+    # # define the video dimensions (720p) and centre
+    # video_dim = np.array([720, 960])
+    # video_centre = video_dim / 2
 
     # find the mean position of the markers
-    marker1 = np.array( [ np.mean(corners[0][:,0]), np.mean(corners[0][:,1]) ] )
-    marker2 = np.array( [ np.mean(corners[1][:,0]), np.mean(corners[1][:,1]) ] )
-    gate_markers = np.array([marker1, marker2])
+    corners1 = np.array(corners[0][0])
+    corners2 = np.array(corners[1][0])
+
+    marker1 = np.array( [ int(np.mean(corners1[:,0])), int(np.mean(corners1[:,1])) ] )
+    marker2 = np.array( [ int(np.mean(corners2[:,0])), int(np.mean(corners2[:,1])) ] )
+    # gate_markers = np.array([marker1, marker2])
 
     # centre of the gate, with markers at two opposite corners
-    gate_centre = np.mean(gate_markers, axis=0)
+    # gate_centre = np.mean(gate_markers, axis=0)
+    gate_centre = np.array([(marker1[0]+marker2[0])//2,(marker1[1]+marker2[1])//2])
+
+
+    # round to nearest pixel
+    gate_centre = np.array([int(gate_centre[0]), int(gate_centre[1])])
+
     print(gate_centre)
 
-    # translation from the video centre to gate centre
-    translate = gate_centre - video_centre
-    return translate
 
-print(gate_position_cf(corners))
+
+    return gate_centre, marker1, marker2
+
+    # # translation from the video centre to gate centre (rounded to whole pixels)
+    # print(f'video centre = {video_centre}, gate centre = {gate_centre}')
+    # translate = gate_centre - video_centre
+
+    # translate = np.array([int(translate[0]),int(translate[1])])
+
+    # return translate
+
+# print(gate_position_cf(corners))
